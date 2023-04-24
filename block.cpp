e803c9567e6a682d9ef885e34e9e42840b806bd7
Akshavya Aggarwal <97899039+Akshavya2509@users.noreply.github.com>
2023-04-24 22:22:00 +0530

    Update block.cpp

--- block.cpp
+++ block.cpp
@@ -1,189 +1,166 @@
-#include<ctime>
-#include<vector>
-#include<iostream>
-using namespace std;
 
-// Transaction Data
-struct TransactionData
-{
-    double amount;
-    string senderkey;
-    string recieverkey;
-    time_t timestamp;
-};
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Date;
 
-// Block class
-class Block
-{
-    private:
-    int index;
-    size_t blockhash;
-    size_t previoushash;
-    size_t generatehash();
+class TransactionData {
+public double amount;
+public String receiverKey;
+public String senderKey;
+public long timeStamp;
 
-    public:
+public TransactionData(double amount, String receiverKey, String senderKey, long timeStamp) {
+    this.amount = amount;
+    this.receiverKey = receiverKey;
+    this.senderKey = senderKey;
+    this.timeStamp = timeStamp;
+}
+     public String getSenderKey() {
+        return this.senderKey;
+    }
 
-    Block(int idx, TransactionData d, size_t prevhash);
+    public void setSenderKey(String senderKey) {
+        this.senderKey = senderKey;
+    }
 
-    size_t gethash();
+    public String getReceiverKey() {
+        return receiverKey;
+    }
 
-    size_t getprevioushash();
+    public void setReceiverKey(String receiverKey) {
+        this.receiverKey = receiverKey;
+    }
 
-    TransactionData data;
+    public int getAmount() {
+        return (int)amount;
+    }
 
-    bool ishashvalid();
-};
-
-// Constructor
-
-Block::Block(int idx, TransactionData d, size_t prevhash)
-{
-    index=idx;
-    data=d;
-    previoushash=prevhash;
-    blockhash=generatehash();
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
 }
 
-size_t Block::generatehash()
-{
-    hash<string> hash1;
-    hash<size_t> hash2;
-    hash<size_t> finalhash;
 
-    string tohash = to_string(data.amount) +data.recieverkey+data.senderkey+to_string(data.timestamp);
+class Block {
+private int index;
+private ArrayList<TransactionData> data;
+private BigInteger previousHash;
+private BigInteger hash;
+private long timeStamp;
+private int nonce;
 
-    return finalhash(hash1(tohash)+hash2(previoushash));
-};
-
-size_t Block::gethash()
-{
-    return blockhash;
+public Block(int index, ArrayList<TransactionData> data, BigInteger previousHash) {
+    this.index = index;
+    this.data = data;
+    this.previousHash = previousHash;
+    this.timeStamp = new Date().getTime();
+    this.nonce = 0;
+    this.hash = calculateHash();
 }
 
-size_t Block::getprevioushash()
-{
-    return previoushash;
+public BigInteger calculateHash() {
+    String dataHash = "";
+    for (TransactionData transaction : data) {
+        String amountStr = Double.toString(transaction.amount);
+        dataHash += amountStr + transaction.receiverKey + transaction.senderKey + transaction.timeStamp;
+    }
+    String blockHash = this.previousHash.toString() + Long.toString(this.timeStamp) + Integer.toString(this.nonce) + dataHash;
+    return new BigInteger(blockHash);
 }
 
-bool Block::ishashvalid()
-{
-    return generatehash()==blockhash;
+
+public void mineBlock(int difficulty) {
+    String target = new String(new char[difficulty]).replace('\0', '0');
+    while (!this.hash.toString(16).substring(0, difficulty).equals(target)) {
+        this.nonce++;
+        this.hash = calculateHash();
+    }
+    System.out.println("Block mined with hash: " + this.hash);
 }
 
-//Blockchain
-
-class Blockchain
-{
-    private:
-    Block creategenesisblock();
-
-    public:
-
-    vector<Block> chain;
-
-    Blockchain();
-
-    void addblock(TransactionData data);
-
-    bool ischainvalid();
-
-    Block *getlatestblock();
-};
-
-Blockchain::Blockchain()
-{
-    Block genesis = creategenesisblock();
-    chain.push_back(genesis);
+public int getIndex() {
+    return index;
 }
 
-Block Blockchain::creategenesisblock()
-{
-    time_t current;
-    TransactionData d;
-    d.amount=0;
-    d.recieverkey="None";
-    d.senderkey="None";
-    d.timestamp=time(&current);
-
-    hash<int> hash1;
-    Block genesis(0,d,hash1(0));
-    return genesis;
+public ArrayList<TransactionData> getData() {
+    return data;
 }
 
-Block *Blockchain::getlatestblock()
-{
-    return &chain.back();
+public BigInteger getPreviousHash() {
+    return previousHash;
 }
 
-void Blockchain::addblock(TransactionData d)
-{
-    int index = (int)chain.size()-1;
-    Block newBlock(index, d, getlatestblock()->gethash());
+public BigInteger getHash() {
+    return hash;
+}
 }
 
-bool Blockchain::ischainvalid()
-{
-    vector<Block>::iterator it;
-    int chainlen=(int)chain.size();
+class Blockchain {
+private ArrayList<Block> chain;
+private int difficulty;
 
-    for(it=chain.begin(); it != chain.end(); it++)
-    {
-        Block currentblock=*it;
-        if(!currentblock.ishashvalid())
-        {
+public Blockchain(int difficulty) {
+    this.chain = new ArrayList<Block>();
+    this.difficulty = difficulty;
+    this.chain.add(createGenesisBlock());
+}
+
+private Block createGenesisBlock() {
+    String genesisData = "This is the beginning of the blockchain!";
+    return new Block(0, new ArrayList<TransactionData>(), new BigInteger("0"));
+}
+
+public Block getLatestBlock() {
+    return this.chain.get(this.chain.size() - 1);
+}
+
+public void addBlock(TransactionData data) {
+    Block newBlock = new Block(this.chain.size(), new ArrayList<TransactionData>() {{ add(data); }}, getLatestBlock().getHash());
+    newBlock.mineBlock(difficulty);
+    this.chain.add(newBlock);
+}
+
+public boolean isChainValid() {
+    for (int i = 1; i < this.chain.size(); i++) {
+        Block currentBlock = this.chain.get(i);
+        Block previousBlock = this.chain.get(i - 1);
+
+        if (!currentBlock.getHash().equals(currentBlock.calculateHash())) {
             return false;
         }
 
-        if(chainlen>1)
-        {
-            Block previousBlock =*(it-1);
-            if(currentblock.getprevioushash()!=previousBlock.gethash())
-            {
-                return false;
-            }
+        if (!currentBlock.getPreviousHash().equals(previousBlock.getHash())) {
+            return false;
         }
     }
-
     return true;
 }
+}
+public class Main {
+    public static void main(String[] args) {
 
-int main()
-{
-    Blockchain TWSCCoin;
+        Blockchain TWSCCoin = new Blockchain(4);
 
-    TransactionData data1;
+        TransactionData data1 = new TransactionData(1.5, "Utkarsh Tripathi", "Akshat Mehra", new Date().getTime());
 
-    time_t datatime;
+        TWSCCoin.addBlock(data1);
 
-    data1.amount=1.5;
-    data1.recieverkey="Utkarsh Tripathi";
-    data1.senderkey="Akshat Mehra";
-    data1.timestamp=time(&datatime);
+        System.out.println("Is chain Valid?");
+        System.out.println(TWSCCoin.isChainValid());
 
-    TWSCCoin.addblock(data1);
+        TransactionData data2 = new TransactionData(1.5, "Akshat Jaimini", "Amit Goyal", new Date().getTime());
 
-    cout << "Is chain Valid?" << endl
-    << TWSCCoin.ischainvalid() <<endl;
+        TWSCCoin.addBlock(data2);
 
-    TransactionData data2;
+        System.out.println("Now is the chain valid?");
+        System.out.println(TWSCCoin.isChainValid());
 
-    time_t data2time;
+        Block hackBlock = TWSCCoin.getLatestBlock();
+        hackBlock.getData().get(0).setAmount(10000);
+        hackBlock.getData().get(0).setReceiverKey("Akshavya - From Crypto Enthusiasts");
 
-    data2.amount=1.5;
-    data2.recieverkey="Akshat Jaimini";
-    data2.senderkey="Amit Goyal";
-    data2.timestamp=time(&data2time);
 
-    TWSCCoin.addblock(data2);
-
-    cout << "Now is the chain valid?" << endl
-    <<TWSCCoin.ischainvalid() << endl;
-    
-    Block *hackBlock = TWSCCoin.getlatestblock();
-    hackBlock->data.amount=10000;
-    hackBlock->data.recieverkey="Akshavya - From Crypto Enthusiasts";
-
-    cout << "Now is the chain still valid ????" << endl;
-
-    cout << TWSCCoin.ischainvalid() << endl;
+        System.out.println("Now is the chain still valid?");
+        System.out.println(TWSCCoin.isChainValid());
+    }
 }
